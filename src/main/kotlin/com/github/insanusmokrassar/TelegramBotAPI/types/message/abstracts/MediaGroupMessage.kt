19c42933d3e429d9dfb66586201221140bcfdaa8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-01-01 18:15:01 +0800

    MediaGroupMessage implementation

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/abstracts/MediaGroupMessage.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/abstracts/MediaGroupMessage.kt
@@ -1,8 +1,9 @@
 package com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts
 
+import com.github.insanusmokrassar.TelegramBotAPI.types.MediaGroupIdentifier
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.abstracts.MediaGroupContent
 
-interface MediaGroupMessage<T: MediaGroupContent> {
-    val mediaGroupId: String
-    val content: T
+interface MediaGroupMessage : Message {
+    val mediaGroupId: MediaGroupIdentifier
+    val content: MediaGroupContent
 }
