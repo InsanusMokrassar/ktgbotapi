0f5ee669d8b0819c0ac5ca3415e9e8f23ea76eea
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-01-11 11:54:51 +0800

    ForwardedMessage contains nullable "from"

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/ForwardedMessage.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/ForwardedMessage.kt
@@ -6,18 +6,19 @@ import com.github.insanusmokrassar.TelegramBotAPI.types.chat.Chat
 sealed class ForwardedMessage {
     abstract val messageId: MessageIdentifier
     abstract val dateOfOriginal: TelegramDate
+    abstract val from: User?
 }
 
 data class CommonForwardedMessage(
     override val messageId: MessageIdentifier,
     override val dateOfOriginal: TelegramDate,
-    val from: User
+    override val from: User
 ) : ForwardedMessage()
 
 data class ForwardedFromChannelMessage(
     override val messageId: MessageIdentifier,
     override val dateOfOriginal: TelegramDate,
-    val from: User?,
+    override val from: User?,
     val channelChat: Chat,
     val signature: String? = null
 ) : ForwardedMessage()
