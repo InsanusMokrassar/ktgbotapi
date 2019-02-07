caa759db21cfc07ad654c3d1d6dd3c3194112a6f
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-02-07 12:03:28 +0800

    add InlineQueryResult photo and gif common and cached realisations

---
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/InlineQueries/InlineQueryResult/abstracts/results/gif/InlineQueryResultGifCached.kt
@@ -0,0 +1,5 @@
+package com.github.insanusmokrassar.TelegramBotAPI.types.InlineQueries.InlineQueryResult.abstracts.results.gif
+
+import com.github.insanusmokrassar.TelegramBotAPI.types.InlineQueries.InlineQueryResult.abstracts.*
+
+interface InlineQueryResultGifCached : InlineQueryResultGifCommon, WithFileIdInlineQueryResult
---
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/InlineQueries/InlineQueryResult/abstracts/results/photo/InlineQueryResultPhoto.kt
@@ -0,0 +1,5 @@
+package com.github.insanusmokrassar.TelegramBotAPI.types.InlineQueries.InlineQueryResult.abstracts.results.photo
+
+import com.github.insanusmokrassar.TelegramBotAPI.types.InlineQueries.InlineQueryResult.abstracts.*
+
+interface InlineQueryResultPhoto : InlineQueryResultPhotoCommon, UrlInlineQueryResult, ThumbedInlineQueryResult, SizedInlineQueryResult
