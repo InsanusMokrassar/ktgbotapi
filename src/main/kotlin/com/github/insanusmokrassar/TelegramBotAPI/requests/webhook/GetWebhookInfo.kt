ce9e141680b2319a4c2a34ed8a2134d9a88dbf27
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-02-27 14:12:39 +0800

    rewrite GetWebhookInfo as class for better work with serialization

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/webhook/GetWebhookInfo.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/webhook/GetWebhookInfo.kt
@@ -2,11 +2,10 @@ package com.github.insanusmokrassar.TelegramBotAPI.requests.webhook
 
 import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.SimpleRequest
 import com.github.insanusmokrassar.TelegramBotAPI.types.WebhookInfo
-import kotlinx.serialization.KSerializer
-import kotlinx.serialization.Serializable
+import kotlinx.serialization.*
 
 @Serializable
-object GetWebhookInfo : SimpleRequest<WebhookInfo> {
+class GetWebhookInfo : SimpleRequest<WebhookInfo> {
     override fun method(): String = "getWebhookInfo"
 
     override fun resultSerializer(): KSerializer<WebhookInfo> = WebhookInfo.serializer()
