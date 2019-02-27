f1836ec2386dabddb3fc69b603750c1ee3611f15
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-02-27 08:52:22 +0800

    add contextual serializer to DataRequest and Request to avoid exceptions

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/send/media/base/DataRequest.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/send/media/base/DataRequest.kt
@@ -1,5 +1,11 @@
 package com.github.insanusmokrassar.TelegramBotAPI.requests.send.media.base
 
 import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.SimpleRequest
+import kotlinx.serialization.ContextSerializer
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
 
+@Serializable(DataRequestSerializer::class)
 interface DataRequest<T: Any> : SimpleRequest<T>
+
+object DataRequestSerializer : KSerializer<DataRequest<*>> by ContextSerializer(DataRequest::class)
