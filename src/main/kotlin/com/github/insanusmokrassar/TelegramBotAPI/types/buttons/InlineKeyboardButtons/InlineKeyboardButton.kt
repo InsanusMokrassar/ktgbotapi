b002f61b7182d67fff95f46dcb9d6699c22a91ed
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-04-20 13:09:47 +0800

    add serializer to InlineKeyboardButton and remove exact serializer for InlineKeyboardMarkup#keyboard

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardButtons/InlineKeyboardButton.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardButtons/InlineKeyboardButton.kt
@@ -1,5 +1,10 @@
 package com.github.insanusmokrassar.TelegramBotAPI.types.buttons.InlineKeyboardButtons
 
+import kotlinx.serialization.*
+
+@Serializable(InlineKeyboardButtonSerializer::class)
 interface InlineKeyboardButton {
     val text: String
 }
+
+object InlineKeyboardButtonSerializer : KSerializer<InlineKeyboardButton> by ContextSerializer(InlineKeyboardButton::class)
--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardMarkup.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardMarkup.kt
@@ -7,10 +7,5 @@ import kotlinx.serialization.internal.ArrayListSerializer
 @Serializable
 data class InlineKeyboardMarkup(
     @SerialName("inline_keyboard")
-    @Serializable(with = KeyboardSerializer::class)
     val keyboard: Matrix<InlineKeyboardButton>
 ) : KeyboardMarkup
-
-object KeyboardSerializer : KSerializer<Matrix<InlineKeyboardButton>> by ArrayListSerializer(
-    ArrayListSerializer(ContextSerializer(InlineKeyboardButton::class))
-)
