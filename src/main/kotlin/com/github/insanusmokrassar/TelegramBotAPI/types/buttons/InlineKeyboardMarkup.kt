bed60720d33a4c01148579064399a94b6247e425
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-04-20 13:14:49 +0800

    InlineKeyboardMarkup using field as serial name for keyboard

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardMarkup.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/buttons/InlineKeyboardMarkup.kt
@@ -1,11 +1,12 @@
 package com.github.insanusmokrassar.TelegramBotAPI.types.buttons
 
 import com.github.insanusmokrassar.TelegramBotAPI.types.buttons.InlineKeyboardButtons.InlineKeyboardButton
+import com.github.insanusmokrassar.TelegramBotAPI.types.inlineKeyboardField
 import kotlinx.serialization.*
 import kotlinx.serialization.internal.ArrayListSerializer
 
 @Serializable
 data class InlineKeyboardMarkup(
-    @SerialName("inline_keyboard")
+    @SerialName(inlineKeyboardField)
     val keyboard: Matrix<InlineKeyboardButton>
 ) : KeyboardMarkup
