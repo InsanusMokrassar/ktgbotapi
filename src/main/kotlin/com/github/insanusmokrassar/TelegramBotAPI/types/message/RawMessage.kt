7977071d1ae21269034071308be46317b9fa38f8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-04-30 12:41:52 +0800

    adapt raw message for forwarded messages

--- src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/RawMessage.kt
+++ src/main/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/message/RawMessage.kt
@@ -134,8 +134,8 @@ data class RawMessage(
     @Transient
     private val forwarded: ForwardedMessage? by lazy {
         forward_date ?: return@lazy null // According to the documentation, now any forwarded message contains this field
-        forward_from_message_id ?.let {
-            forward_from ?: throw IllegalStateException("For common forwarded messages author of original message declared as set up required")
+        forward_from ?.let {
+            forward_from_message_id ?: throw IllegalStateException("For common forwarded messages message id of the original message declared as set up required")
             forward_from_chat ?.let {
                 ForwardedFromChannelMessage(
                     forward_from_message_id,
