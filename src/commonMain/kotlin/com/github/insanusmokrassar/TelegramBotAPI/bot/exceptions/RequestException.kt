a68c31f9d1a4283890a250188546f471c55bd172
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2019-12-13 23:55:06 +0600

    0.20.2 - MessageToEditNotFoundException

--- src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/bot/exceptions/RequestException.kt
+++ src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/bot/exceptions/RequestException.kt
@@ -11,6 +11,7 @@ fun newRequestException(
 ) = response.description ?.let { description ->
     when {
         description == "Bad Request: reply message not found" -> ReplyMessageNotFoundException(response, plainAnswer, message, cause)
+        description == "Bad Request: message to edit not found" -> MessageToEditNotFoundException(response, plainAnswer, message, cause)
         description.contains("Bad Request: message is not modified") -> MessageIsNotModifiedException(response, plainAnswer, message, cause)
         description == "Unauthorized" -> UnauthorizedException(response, plainAnswer, message, cause)
         else -> null
@@ -37,4 +38,7 @@ class ReplyMessageNotFoundException(response: Response, plainAnswer: String, mes
     RequestException(response, plainAnswer, message, cause)
 
 class MessageIsNotModifiedException(response: Response, plainAnswer: String, message: String?, cause: Throwable?) :
+    RequestException(response, plainAnswer, message, cause)
+
+class MessageToEditNotFoundException(response: Response, plainAnswer: String, message: String?, cause: Throwable?) :
     RequestException(response, plainAnswer, message, cause)
