00886dcfb75ff193a5ac25ae6bc81dc786fc59ef
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-01-23 19:31:09 +0600

    now user have correct serializer

--- src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/User.kt
+++ src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/types/User.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.*
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObjectSerializer
 
-@Serializable
+@Serializable(UserSerializer::class)
 sealed class User : PrivateChat
 
 @Serializable
