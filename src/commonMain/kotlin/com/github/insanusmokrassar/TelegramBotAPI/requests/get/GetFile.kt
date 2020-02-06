86f1e3ea365871b2e894b08b2b3e0fd5ee2f4608
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-02-06 23:12:49 +0600

    add get file info extensions

--- src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/get/GetFile.kt
+++ src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/get/GetFile.kt
@@ -1,8 +1,10 @@
 package com.github.insanusmokrassar.TelegramBotAPI.requests.get
 
+import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.FileId
 import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.SimpleRequest
 import com.github.insanusmokrassar.TelegramBotAPI.types.files.PathedFile
+import com.github.insanusmokrassar.TelegramBotAPI.types.files.abstracts.TelegramMediaFile
 import com.github.insanusmokrassar.TelegramBotAPI.types.files.abstracts.fileIdField
 import kotlinx.serialization.*
 
@@ -17,3 +19,13 @@ data class GetFile(
     override val requestSerializer: SerializationStrategy<*>
         get() = serializer()
 }
+
+suspend fun RequestsExecutor.getFileAdditionalInfo(
+    fileId: FileId
+) = execute(
+    GetFile(fileId)
+)
+
+suspend fun RequestsExecutor.getFileAdditionalInfo(
+    file: TelegramMediaFile
+) = getFileAdditionalInfo(file.fileId)
