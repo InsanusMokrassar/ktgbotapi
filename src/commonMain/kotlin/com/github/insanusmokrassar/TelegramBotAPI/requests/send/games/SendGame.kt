dcfa198c8cca94eda02d1a34d9601e5bc65eff58
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-02-07 00:25:24 +0600

    send game and other extensions

--- src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/send/games/SendGame.kt
+++ src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/requests/send/games/SendGame.kt
@@ -1,9 +1,12 @@
 package com.github.insanusmokrassar.TelegramBotAPI.requests.send.games
 
 import com.github.insanusmokrassar.TelegramBotAPI.CommonAbstracts.types.ReplyMarkup
+import com.github.insanusmokrassar.TelegramBotAPI.bot.RequestsExecutor
 import com.github.insanusmokrassar.TelegramBotAPI.requests.send.abstracts.SendMessageRequest
 import com.github.insanusmokrassar.TelegramBotAPI.types.*
 import com.github.insanusmokrassar.TelegramBotAPI.types.buttons.KeyboardMarkup
+import com.github.insanusmokrassar.TelegramBotAPI.types.chat.abstracts.Chat
+import com.github.insanusmokrassar.TelegramBotAPI.types.games.Game
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.ContentMessage
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.TelegramBotAPIMessageDeserializationStrategyClass
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.GameContent
@@ -32,3 +35,45 @@ data class SendGame (
     override val requestSerializer: SerializationStrategy<*>
         get() = serializer()
 }
+
+suspend fun RequestsExecutor.sendGame(
+    chatId: ChatIdentifier,
+    gameShortName: String,
+    disableNotification: Boolean = false,
+    replyToMessageId: MessageIdentifier? = null,
+    replyMarkup: KeyboardMarkup? = null
+) = execute(
+    SendGame(
+        chatId, gameShortName, disableNotification, replyToMessageId, replyMarkup
+    )
+)
+
+suspend fun RequestsExecutor.sendGame(
+    chat: Chat,
+    gameShortName: String,
+    disableNotification: Boolean = false,
+    replyToMessageId: MessageIdentifier? = null,
+    replyMarkup: KeyboardMarkup? = null
+) = sendGame(
+    chat.id, gameShortName, disableNotification, replyToMessageId, replyMarkup
+)
+
+suspend fun RequestsExecutor.sendGame(
+    chatId: ChatIdentifier,
+    game: Game,
+    disableNotification: Boolean = false,
+    replyToMessageId: MessageIdentifier? = null,
+    replyMarkup: KeyboardMarkup? = null
+) = sendGame(
+    chatId, game.title, disableNotification, replyToMessageId, replyMarkup
+)
+
+suspend fun RequestsExecutor.sendGame(
+    chat: Chat,
+    game: Game,
+    disableNotification: Boolean = false,
+    replyToMessageId: MessageIdentifier? = null,
+    replyMarkup: KeyboardMarkup? = null
+) = sendGame(
+    chat.id, game.title, disableNotification, replyToMessageId, replyMarkup
+)
