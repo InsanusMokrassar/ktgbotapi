be64287c42efa12b8337b6beb10c89ec2b9f6dfd
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-03-17 19:50:25 +0600

    copypaste functions from TelegramBotAPI to TelegramBotAPI-extensions-api with adaptation of updates

---
+++ TelegramBotAPI-extensions-api/src/commonMain/kotlin/com/github/insanusmokrassar/TelegramBotAPI/extensions/api/InternalUtils/UpdatesAdapter.kt
@@ -0,0 +1,51 @@
+package com.github.insanusmokrassar.TelegramBotAPI.extensions.api.InternalUtils
+
+import com.github.insanusmokrassar.TelegramBotAPI.types.MediaGroupIdentifier
+import com.github.insanusmokrassar.TelegramBotAPI.types.message.abstracts.MediaGroupMessage
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.*
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.MediaGroupUpdates.*
+import com.github.insanusmokrassar.TelegramBotAPI.types.update.abstracts.*
+
+internal fun List<BaseMessageUpdate>.convertWithMediaGroupUpdates(): List<Update> {
+    val resultUpdates = mutableListOf<Update>()
+    val mediaGroups = mutableMapOf<MediaGroupIdentifier, MutableList<BaseMessageUpdate>>()
+    for (update in this) {
+        val asEditMediaGroupMessage = update.toEditMediaGroupUpdate()
+        if (asEditMediaGroupMessage != null) {
+            resultUpdates.add(asEditMediaGroupMessage)
+        } else {
+            val data = update.data
+            if (data is MediaGroupMessage) {
+                (mediaGroups[data.mediaGroupId] ?: mutableListOf<BaseMessageUpdate>().also { mediaGroups[data.mediaGroupId] = it }).add(update)
+            } else {
+                resultUpdates.add(update)
+            }
+        }
+    }
+    mediaGroups.values.map {
+        it.toSentMediaGroupUpdate() ?.let { mediaGroupUpdate ->
+            resultUpdates.add(mediaGroupUpdate)
+        }
+    }
+    return resultUpdates.sortedBy { it.updateId }
+}
+
+internal fun List<BaseMessageUpdate>.toSentMediaGroupUpdate(): SentMediaGroupUpdate? = (this as? SentMediaGroupUpdate) ?: let {
+    if (isEmpty()) {
+        return@let null
+    }
+    val resultList = sortedBy { it.updateId }
+    when (first()) {
+        is MessageUpdate -> MessageMediaGroupUpdate(resultList)
+        is ChannelPostUpdate -> ChannelPostMediaGroupUpdate(resultList)
+        else -> null
+    }
+}
+
+internal fun BaseMessageUpdate.toEditMediaGroupUpdate(): EditMediaGroupUpdate? = (this as? EditMediaGroupUpdate) ?: let {
+    when (this) {
+        is EditMessageUpdate -> EditMessageMediaGroupUpdate(this)
+        is EditChannelPostUpdate -> EditChannelPostMediaGroupUpdate(this)
+        else -> null
+    }
+}
