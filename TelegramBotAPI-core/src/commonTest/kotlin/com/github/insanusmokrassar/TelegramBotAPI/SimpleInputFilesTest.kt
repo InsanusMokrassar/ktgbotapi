6788c4d1ddbe7681645af6644ec85cd2ce182d97
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2020-08-24 00:34:30 +0600

    make the test failing again

--- TelegramBotAPI-core/src/commonTest/kotlin/com/github/insanusmokrassar/TelegramBotAPI/SimpleInputFilesTest.kt
+++ TelegramBotAPI-core/src/commonTest/kotlin/com/github/insanusmokrassar/TelegramBotAPI/SimpleInputFilesTest.kt
@@ -1,10 +1,18 @@
 package com.github.insanusmokrassar.TelegramBotAPI
 
 import com.github.insanusmokrassar.TelegramBotAPI.requests.abstracts.toInputFile
-import com.github.insanusmokrassar.TelegramBotAPI.types.files.Photo
+import com.github.insanusmokrassar.TelegramBotAPI.types.InputMedia.MediaGroupMemberInputMediaSerializer
 import com.github.insanusmokrassar.TelegramBotAPI.types.files.PhotoSize
 import com.github.insanusmokrassar.TelegramBotAPI.types.message.content.media.PhotoContent
+import kotlinx.serialization.json.Json
 import kotlin.test.Test
+
+private val nonstrictJsonFormat = Json {
+    isLenient = true
+    ignoreUnknownKeys = true
+    allowSpecialFloatingPointValues = true
+    useArrayPolymorphism = true
+}
 
 class SimpleInputFilesTest {
     @Test
@@ -14,6 +22,9 @@ class SimpleInputFilesTest {
                 PhotoSize("example_file_id".toInputFile(), "example_unique_file_id", 100, 100, 100)
             )
         )
-        photoContent.toMediaGroupMemberInputMedia()
+        nonstrictJsonFormat.encodeToString(
+            MediaGroupMemberInputMediaSerializer,
+            photoContent.toMediaGroupMemberInputMedia()
+        )
     }
 }
