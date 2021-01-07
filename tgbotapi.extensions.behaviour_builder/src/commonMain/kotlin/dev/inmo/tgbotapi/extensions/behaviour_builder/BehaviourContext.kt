a28cf5ddff4091d2447b07e35b8673b87d456736
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-01-07 18:20:23 +0600

    now BehaviourContext is UpdateFilterm TelegramBot and CoroutineScope

--- tgbotapi.extensions.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/BehaviourContext.kt
+++ tgbotapi.extensions.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/BehaviourContext.kt
@@ -2,6 +2,7 @@ package dev.inmo.tgbotapi.extensions.behaviour_builder
 
 import dev.inmo.tgbotapi.bot.TelegramBot
 import dev.inmo.tgbotapi.updateshandlers.FlowsUpdatesFilter
+import dev.inmo.tgbotapi.updateshandlers.UpdatesFilter
 import kotlinx.coroutines.CoroutineScope
 
 typealias BehaviourContextReceiver<T> = suspend BehaviourContext.() -> T
@@ -11,4 +12,4 @@ data class BehaviourContext(
     val bot: TelegramBot,
     val scope: CoroutineScope,
     val flowsUpdatesFilter: FlowsUpdatesFilter = FlowsUpdatesFilter()
-)
+) : UpdatesFilter by flowsUpdatesFilter, TelegramBot by bot, CoroutineScope by scope
