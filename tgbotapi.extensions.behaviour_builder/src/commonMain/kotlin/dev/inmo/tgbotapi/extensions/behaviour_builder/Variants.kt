08b87107729990a9d7fcf44ef7172940e4da47d5
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-01-19 20:18:30 +0600

    add asAction and several variants of oneOf

--- tgbotapi.extensions.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/Variants.kt
+++ tgbotapi.extensions.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/Variants.kt
@@ -12,10 +12,20 @@ suspend fun <T> BehaviourContext.parallel(
 
 inline infix fun <T, O> Deferred<T>.withAction(noinline callback: suspend (T) -> O) = DeferredAction(this, callback)
 
-suspend fun <O> BehaviourContext.oneOf(
+inline fun <T> Deferred<T>.asAction() = DeferredAction(this) { it }
+
+suspend fun <O> BehaviourContext.oneOfActions(
     deferredActions: Iterable<DeferredAction<*, O>>
 ) = deferredActions.invokeFirstOf(scope)
 
-suspend fun <O> BehaviourContext.oneOf(
+suspend fun <O> BehaviourContext.oneOfActions(
     vararg deferredActions: DeferredAction<*, O>
+) = this@oneOfActions.oneOfActions(deferredActions.toList())
+
+suspend fun <O> BehaviourContext.oneOf(
+    deferredActions: Iterable<Deferred<O>>
+) = oneOfActions(deferredActions.map { it.asAction() })
+
+suspend fun <O> BehaviourContext.oneOf(
+    vararg deferredActions: Deferred<O>
 ) = oneOf(deferredActions.toList())
