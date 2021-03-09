8bb60bea341384fe90d1b667bcc05c67f2b2a279
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-03-09 21:37:25 +0600

    now MessageAutoDeleteTimerChanged implementing ChannelEvent and GroupEvent

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/ChatEvents/MessageAutoDeleteTimerChanged.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/ChatEvents/MessageAutoDeleteTimerChanged.kt
@@ -1,11 +1,10 @@
 package dev.inmo.tgbotapi.types.message.ChatEvents
 
 import dev.inmo.tgbotapi.types.Seconds
-import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.ChannelEvent
-import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.GroupEvent
+import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.*
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class MessageAutoDeleteTimerChanged(
     val newAutoDeleteTime: Seconds // TODO:: check that it is seconds
-) : ChannelEvent, GroupEvent
+) : ChannelEvent, GroupEvent, SupergroupEvent
