919b1590e39d52189be8a76bb10f88e57340a853
vasya <t.lila@list.ru>
2021-06-28 20:30:12 +0300

    add Update#sourceUser method

--- tgbotapi.extensions.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/UpdateChatRetriever.kt
+++ tgbotapi.extensions.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/UpdateChatRetriever.kt
@@ -1,6 +1,8 @@
 package dev.inmo.tgbotapi.extensions.utils.extensions
 
+import dev.inmo.tgbotapi.extensions.utils.asUser
 import dev.inmo.tgbotapi.extensions.utils.shortcuts.chat
+import dev.inmo.tgbotapi.types.User
 import dev.inmo.tgbotapi.types.chat.abstracts.Chat
 import dev.inmo.tgbotapi.types.update.*
 import dev.inmo.tgbotapi.types.update.MediaGroupUpdates.*
@@ -16,6 +18,23 @@ fun Update.sourceChat(): Chat? = when (this) {
         else -> null
     }
     is BaseMessageUpdate -> data.chat
+    is InlineQueryUpdate -> data.from
+    is ChosenInlineResultUpdate -> data.user
+    is CallbackQueryUpdate -> data.user
+    is PreCheckoutQueryUpdate -> data.user
+    is PollAnswerUpdate -> data.user
+    is ShippingQueryUpdate -> data.user
+    else -> null
+}
+
+@PreviewFeature
+fun Update.sourceUser(): User? = when (this) {
+    is MediaGroupUpdate -> when (this) {
+        is SentMediaGroupUpdate -> data.chat?.asUser()
+        is EditMediaGroupUpdate -> data.chat.asUser()
+        else -> null
+    }
+    is BaseMessageUpdate -> data.chat.asUser()
     is InlineQueryUpdate -> data.from
     is ChosenInlineResultUpdate -> data.user
     is CallbackQueryUpdate -> data.user
