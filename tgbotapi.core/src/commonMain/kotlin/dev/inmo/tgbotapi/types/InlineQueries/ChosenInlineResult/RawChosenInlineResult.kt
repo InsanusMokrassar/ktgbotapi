38826a5e7ef2a5d8e3ca1177d653b69373264ca6
vasya <t.lila@list.ru>
2021-06-29 21:06:46 +0300

    remove FromUser interface from RawChosenInlineResult

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/InlineQueries/ChosenInlineResult/RawChosenInlineResult.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/InlineQueries/ChosenInlineResult/RawChosenInlineResult.kt
@@ -1,6 +1,5 @@
 package dev.inmo.tgbotapi.types.InlineQueries.ChosenInlineResult
 
-import dev.inmo.tgbotapi.CommonAbstracts.FromUser
 import dev.inmo.tgbotapi.types.*
 import dev.inmo.tgbotapi.types.InlineQueries.abstracts.ChosenInlineResult
 import dev.inmo.tgbotapi.types.location.StaticLocation
@@ -12,14 +11,14 @@ internal data class RawChosenInlineResult(
     @SerialName(resultIdField)
     val resultId: InlineQueryIdentifier, //chosen temporary, can be changed
     @SerialName(fromField)
-    override val user: User,
+    val user: User,
     @SerialName(queryField)
     val query: String,
     @SerialName(locationField)
     val location: StaticLocation? = null,
     @SerialName(inlineMessageIdField)
     val inlineMessageId: InlineMessageIdentifier? = null
-) : FromUser {
+) {
     val asChosenInlineResult: ChosenInlineResult by lazy {
         location ?.let {
             LocationChosenInlineResult(resultId, user, location, inlineMessageId, query)
