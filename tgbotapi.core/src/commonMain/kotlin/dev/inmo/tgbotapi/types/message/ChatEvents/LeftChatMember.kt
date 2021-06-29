c00adb0784184bd022f625248d4c078e09754f43
vasya <t.lila@list.ru>
2021-06-29 21:07:18 +0300

    remove redundant import in LeftChatMember.kt

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/ChatEvents/LeftChatMember.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/ChatEvents/LeftChatMember.kt
@@ -1,6 +1,5 @@
 package dev.inmo.tgbotapi.types.message.ChatEvents
 
-import dev.inmo.tgbotapi.CommonAbstracts.FromUser
 import dev.inmo.tgbotapi.types.User
 import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.CommonEvent
 
