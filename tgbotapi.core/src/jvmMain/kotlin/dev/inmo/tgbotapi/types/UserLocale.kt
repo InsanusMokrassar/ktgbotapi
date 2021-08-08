95abc72bf3b58250a8a1dde29bd885d62d3e6e8a
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-08-08 17:47:18 +0600

    WithOptionalLanguageCode?.javaLocale

--- tgbotapi.core/src/jvmMain/kotlin/dev/inmo/tgbotapi/types/UserLocale.kt
+++ tgbotapi.core/src/jvmMain/kotlin/dev/inmo/tgbotapi/types/UserLocale.kt
@@ -1,8 +1,8 @@
 package dev.inmo.tgbotapi.types
 
 import dev.inmo.micro_utils.language_codes.IetfLanguageCode
-import dev.inmo.tgbotapi.types.abstracts.WithLanguageCode
+import dev.inmo.tgbotapi.types.abstracts.WithOptionalLanguageCode
 import java.util.*
 
 fun IetfLanguageCode?.javaLocale() = this ?.code ?.let { Locale.forLanguageTag(it) }
-fun WithLanguageCode?.javaLocale() = this ?.ietfLanguageCode.javaLocale()
+fun WithOptionalLanguageCode?.javaLocale() = this ?.ietfLanguageCode ?.javaLocale()
