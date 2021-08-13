f147a3d620259b9b574fd855c5cfa4c809947bfb
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-08-13 11:41:57 +0600

    fix of package for AdditionalContentAsStorageFile

--- tgbotapi.extensions.utils/src/jvmMain/kotlin/dev/inmo/tgbotapi/extensions/utils/types/files/AdditionalContentAsStorageFile.kt
+++ tgbotapi.extensions.utils/src/jvmMain/kotlin/dev/inmo/tgbotapi/extensions/utils/types/files/AdditionalContentAsStorageFile.kt
@@ -1,9 +1,10 @@
-package dev.inmo.tgbotapi.types.files
+package dev.inmo.tgbotapi.extensions.utils.types.files
 
 import dev.inmo.tgbotapi.bot.TelegramBot
 import dev.inmo.tgbotapi.requests.DownloadFileStream
 import dev.inmo.tgbotapi.requests.abstracts.FileId
 import dev.inmo.tgbotapi.requests.get.GetFile
+import dev.inmo.tgbotapi.types.files.PathedFile
 import dev.inmo.tgbotapi.types.files.abstracts.TelegramMediaFile
 import dev.inmo.tgbotapi.types.message.content.abstracts.MediaContent
 import dev.inmo.tgbotapi.utils.*
