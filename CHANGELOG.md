226b8dee2113dd9b9f0aec9cae2f78e9d35e0eca
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-08-22 23:13:59 +0600

    deprecate allSentMessagesFlow and allSentMedaGroupsFlow

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@
     * `Version`:
         * `Klock`: `2.3.1` -> `2.3.3`
         * `MicroUtils`: `0.5.19` -> `0.5.21`
+* `Utils`:
+    * Extensions `allSentMessagesFlow` and `allSentMediaGroupsFlow` have been deprecated
 
 ## 0.35.5
 
--- tgbotapi.extensions.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/FlowsUpdatesFilter.kt
+++ tgbotapi.extensions.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/FlowsUpdatesFilter.kt
@@ -6,14 +6,16 @@ import dev.inmo.tgbotapi.updateshandlers.FlowsUpdatesFilter
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.merge
 
+@Deprecated("Will be removed soon", ReplaceWith("messageFlow + channelPostFlow"))
 val FlowsUpdatesFilter.allSentMessagesFlow: Flow<BaseSentMessageUpdate>
     get() = merge(
-        messageFlow,
-        channelPostFlow
+        messagesFlow,
+        channelPostsFlow
     )
 
+@Deprecated("Will be removed soon", ReplaceWith("messageMediaGroupFlow + channelPostMediaGroupFlow"))
 val FlowsUpdatesFilter.allSentMediaGroupsFlow: Flow<SentMediaGroupUpdate>
     get() = merge(
-        messageMediaGroupFlow,
-        channelPostMediaGroupFlow
+        messageMediaGroupsFlow,
+        channelPostMediaGroupsFlow
     )
