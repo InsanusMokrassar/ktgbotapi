1e5ce6bb5c246012c55f5579a6968a9994665546
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2021-12-18 23:12:12 +0600

    add ContentMessage#hasProtectedContent

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/abstracts/ContentMessage.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/abstracts/ContentMessage.kt
@@ -5,4 +5,7 @@ import dev.inmo.tgbotapi.types.message.content.abstracts.MessageContent
 interface ContentMessage<T: MessageContent>: Message {
     val forwardable: Boolean
     val content: T
+
+    val hasProtectedContent: Boolean
+        get() = forwardable
 }
