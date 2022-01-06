953638ddf55290fd9d4fc55d608c7e4443483b7c
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-01-06 16:57:51 +0600

    add raw fields for CallbackQuery

---
+++ tgbotapi.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/raw/CallbackQuery.kt
@@ -0,0 +1,23 @@
+package dev.inmo.tgbotapi.extensions.utils.extensions.raw
+
+import dev.inmo.tgbotapi.extensions.utils.*
+import dev.inmo.tgbotapi.types.CallbackQuery.CallbackQuery
+import dev.inmo.tgbotapi.types.InlineMessageIdentifier
+import dev.inmo.tgbotapi.types.User
+import dev.inmo.tgbotapi.types.message.abstracts.ContentMessage
+import dev.inmo.tgbotapi.types.message.abstracts.Message
+import dev.inmo.tgbotapi.types.message.content.abstracts.MessageContent
+import dev.inmo.tgbotapi.utils.RiskFeature
+
+@RiskFeature(RawFieldsUsageWarning)
+val CallbackQuery.message: ContentMessage<MessageContent>?
+    get() = asMessageCallbackQuery() ?.message
+@RiskFeature(RawFieldsUsageWarning)
+val CallbackQuery.inline_message_id: InlineMessageIdentifier?
+    get() = asInlineMessageIdCallbackQuery() ?.inlineMessageId
+@RiskFeature(RawFieldsUsageWarning)
+val CallbackQuery.data: String?
+    get() = asDataCallbackQuery() ?.data
+@RiskFeature(RawFieldsUsageWarning)
+val CallbackQuery.game_short_name: String?
+    get() = asGameShortNameCallbackQuery() ?.gameShortName
