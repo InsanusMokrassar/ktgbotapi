4ffe6f915f539b07c78d1e8848567b88b991d31e
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-04-17 20:30:18 +0600

    add support of SentWebAppMessage

---
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/requests/answers/AnswerWebAppQuery.kt
@@ -0,0 +1,21 @@
+package dev.inmo.tgbotapi.requests.answers
+
+import dev.inmo.tgbotapi.requests.abstracts.SimpleRequest
+import dev.inmo.tgbotapi.types.*
+import dev.inmo.tgbotapi.types.InlineQueries.InlineQueryResult.abstracts.InlineQueryResult
+import dev.inmo.tgbotapi.types.webapps.query.SentWebAppMessage
+import kotlinx.serialization.*
+
+@Serializable
+data class AnswerWebAppQuery(
+    @SerialName(webAppQueryIdField)
+    val webAppQueryId: WebAppQueryId,
+    @SerialName(resultField)
+    val result: InlineQueryResult
+) : SimpleRequest<SentWebAppMessage> {
+    override fun method(): String = "answerWebAppQuery"
+    override val resultDeserializer: DeserializationStrategy<SentWebAppMessage>
+        get() = SentWebAppMessage.serializer()
+    override val requestSerializer: SerializationStrategy<*>
+        get() = serializer()
+}
