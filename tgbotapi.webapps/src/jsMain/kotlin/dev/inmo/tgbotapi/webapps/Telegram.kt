0e6a708eeaa93be204d2f01d599e8c2c81d16aec
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-04-19 15:17:07 +0600

    add shortcuts for telegram and webApp in webapps

--- tgbotapi.webapps/src/jsMain/kotlin/dev/inmo/tgbotapi/webapps/Telegram.kt
+++ tgbotapi.webapps/src/jsMain/kotlin/dev/inmo/tgbotapi/webapps/Telegram.kt
@@ -1,5 +1,6 @@
 package dev.inmo.tgbotapi.webapps
 
+import kotlinx.browser.window
 import org.w3c.dom.Window
 
 external interface Telegram {
@@ -8,3 +9,9 @@ external interface Telegram {
 
 val Window.Telegram
     get() = asDynamic().Telegram.unsafeCast<Telegram>()
+
+val telegram: Telegram
+    get() = window.Telegram
+
+val webApp: WebApp
+    get() = telegram.WebApp
