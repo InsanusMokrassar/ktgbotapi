60c21002e18d2dbe91547fd3e27bb1011de312d8
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-04-29 19:50:02 +0600

    fixes

--- tgbotapi.core/src/jvmMain/kotlin/dev/inmo/tgbotapi/utils/CryptoActual.kt
+++ tgbotapi.core/src/jvmMain/kotlin/dev/inmo/tgbotapi/utils/CryptoActual.kt
@@ -1,21 +1,8 @@
 package dev.inmo.tgbotapi.utils
 
-import dev.inmo.micro_utils.crypto.SourceBytes
 import dev.inmo.micro_utils.crypto.SourceString
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-
-val HEX_ARRAY = "0123456789ABCDEF".toCharArray()
-
-private fun SourceBytes.hex(): String {
-    val hexChars = CharArray(size * 2)
-    for (j in indices) {
-        val v: Int = this[j].toInt() and 0xFF
-        hexChars[j * 2] = HEX_ARRAY[v ushr 4]
-        hexChars[j * 2 + 1] = HEX_ARRAY[v and 0x0F]
-    }
-    return String(hexChars)
-}
 
 actual fun SourceString.hmacSha256(key: String): String {
     val mac = Mac.getInstance("HmacSHA256")
@@ -25,5 +12,3 @@ actual fun SourceString.hmacSha256(key: String): String {
 
     return mac.doFinal(toByteArray()).hex()
 }
-
-actual fun SourceString.hex(): String = toByteArray().hex()
