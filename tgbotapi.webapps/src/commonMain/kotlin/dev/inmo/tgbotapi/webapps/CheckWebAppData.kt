78a7a3546a4c291528778c89485fa23f4ad477d0
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-04-29 19:18:22 +0600

    remove checking of webapp data from webapp module due to its existing in core

--- tgbotapi.webapps/src/commonMain/kotlin/dev/inmo/tgbotapi/webapps/CheckWebAppData.kt
+++
@@ -1,9 +0,0 @@
-package dev.inmo.tgbotapi.webapps
-
-import dev.inmo.tgbotapi.utils.*
-
-/**
- * @param rawData Data from [dev.inmo.tgbotapi.webapps.WebApp.initData]
- * @param hash Data from [dev.inmo.tgbotapi.webapps.WebApp.initDataUnsafe] from the field [dev.inmo.tgbotapi.webapps.WebAppInitData.hash]
- */
-fun TelegramAPIUrlsKeeper.checkWebAppLink(rawData: String, hash: String) = rawData.hmacSha256(webAppDataSecretKey).hex() == hash
