edbfedb74e888e2201e2c78892e4fb69990cd6d2
d1snin <me@d1s.dev>
2022-07-18 11:26:18 +0300

    added logically missing typealiases

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/content/Typealiases.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/message/content/Typealiases.kt
@@ -2,20 +2,32 @@ package dev.inmo.tgbotapi.types.message.content
 
 import dev.inmo.tgbotapi.types.message.abstracts.CommonMessage
 
-typealias ContactMessage = CommonMessage<ContactContent>
-typealias VenueMessage = CommonMessage<VenueContent>
-typealias PollMessage = CommonMessage<PollContent>
-typealias DiceMessage = CommonMessage<DiceContent>
-typealias GameMEssage = CommonMessage<GameContent>
-typealias TextMessage = CommonMessage<TextContent>
-typealias LocationMessage = CommonMessage<LocationContent>
-typealias PhotoMessage = CommonMessage<PhotoContent>
-typealias VideoMessage = CommonMessage<VideoContent>
-typealias AudioMessage = CommonMessage<AudioContent>
-typealias DocumentMessage = CommonMessage<DocumentContent>
-typealias VoiceMessage = CommonMessage<VoiceContent>
-typealias VideoNoteMessage = CommonMessage<VideoNoteContent>
-typealias AnimationMessage = CommonMessage<AnimationContent>
-typealias StickerMessage = CommonMessage<StickerContent>
 typealias InvoiceMessage = CommonMessage<InvoiceContent>
+typealias VenueMessage = CommonMessage<VenueContent>
+typealias GameMessage = CommonMessage<GameContent>
+typealias DiceMessage = CommonMessage<DiceContent>
+typealias ContactMessage = CommonMessage<ContactContent>
+typealias PollMessage = CommonMessage<PollContent>
+typealias TextMessage = CommonMessage<TextContent>
+
+typealias LocationMessage = CommonMessage<LocationContent>
+typealias LiveLocationMessage = CommonMessage<LiveLocationContent>
+typealias StaticLocationMessage = CommonMessage<StaticLocationContent>
+
 typealias MediaCollectionMessage<T> = CommonMessage<MediaCollectionContent<T>>
+typealias MediaMessage = CommonMessage<MediaContent>
+typealias VideoNoteMessage = CommonMessage<VideoNoteContent>
+typealias StickerMessage = CommonMessage<StickerContent>
+typealias TextedMediaMessage = CommonMessage<TextedMediaContent>
+typealias VoiceMessage = CommonMessage<VoiceContent>
+typealias MediaGroupMessage = CommonMessage<MediaGroupContent>
+typealias AudioMediaGroupMessage = CommonMessage<AudioMediaGroupContent>
+typealias AudioMessage = CommonMessage<AudioContent>
+typealias DocumentMediaGroupMessage = CommonMessage<DocumentMediaGroupContent>
+typealias DocumentMessage = CommonMessage<DocumentContent>
+typealias VisualMediaGroupMessage = CommonMessage<VisualMediaGroupContent>
+typealias VideoMessage = CommonMessage<VideoContent>
+typealias PhotoMessage = CommonMessage<PhotoContent>
+typealias AnimationMessage = CommonMessage<AnimationContent>
+
+
