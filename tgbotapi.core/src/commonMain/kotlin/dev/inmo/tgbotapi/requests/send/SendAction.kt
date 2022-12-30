b064becb8a974490c560a37004fd320ac821b260
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2022-12-30 20:49:47 +0600

    add support of topics for actions

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/requests/send/SendAction.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/requests/send/SendAction.kt
@@ -1,5 +1,6 @@
 package dev.inmo.tgbotapi.requests.send
 
+import dev.inmo.tgbotapi.requests.send.abstracts.OptionallyMessageThreadRequest
 import dev.inmo.tgbotapi.requests.send.abstracts.SendChatMessageRequest
 import dev.inmo.tgbotapi.types.*
 import dev.inmo.tgbotapi.types.actions.BotAction
@@ -14,8 +15,10 @@ data class SendAction(
     @SerialName(chatIdField)
     override val chatId: ChatIdentifier,
     @SerialName(actionField)
-    val action: BotAction
-): SendChatMessageRequest<Boolean> {
+    val action: BotAction,
+    @SerialName(messageThreadIdField)
+    override val threadId: MessageThreadId? = chatId.threadId
+): SendChatMessageRequest<Boolean>, OptionallyMessageThreadRequest {
     override fun method(): String = "sendChatAction"
     override val resultDeserializer: DeserializationStrategy<Boolean>
         get() = Boolean.serializer()
