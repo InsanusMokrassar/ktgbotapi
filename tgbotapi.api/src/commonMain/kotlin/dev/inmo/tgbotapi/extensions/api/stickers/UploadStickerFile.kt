4390c12180815746cea59ba62966488d07b1abf0
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2023-03-10 14:24:42 +0600

    update uploadStickerFile

--- tgbotapi.api/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/api/stickers/UploadStickerFile.kt
+++ tgbotapi.api/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/api/stickers/UploadStickerFile.kt
@@ -3,19 +3,22 @@ package dev.inmo.tgbotapi.extensions.api.stickers
 import dev.inmo.tgbotapi.bot.TelegramBot
 import dev.inmo.tgbotapi.requests.abstracts.MultipartFile
 import dev.inmo.tgbotapi.requests.stickers.UploadStickerFile
+import dev.inmo.tgbotapi.types.StickerFormat
 import dev.inmo.tgbotapi.types.chat.CommonUser
 import dev.inmo.tgbotapi.types.UserId
 
 suspend fun TelegramBot.uploadStickerFile(
     userId: UserId,
-    sticker: MultipartFile
+    sticker: MultipartFile,
+    stickerFormat: StickerFormat
 ) = execute(
-    UploadStickerFile(userId, sticker)
+    UploadStickerFile(userId, sticker, stickerFormat)
 )
 
 suspend fun TelegramBot.uploadStickerFile(
     user: CommonUser,
-    sticker: MultipartFile
+    sticker: MultipartFile,
+    stickerFormat: StickerFormat
 ) = execute(
-    UploadStickerFile(user.id, sticker)
+    UploadStickerFile(user.id, sticker, stickerFormat)
 )
