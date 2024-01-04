52fd55eea594cc83a96750f9a832f937dd2c498c
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-01-04 14:39:32 +0600

    fix of overloads conflict in ClassCastsNew

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayResults.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayResults.kt
@@ -3,11 +3,9 @@ package dev.inmo.tgbotapi.types.giveaway
 import dev.inmo.tgbotapi.abstracts.WithPreviewChat
 import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.ChatEvent
 import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.PublicChatEvent
-import dev.inmo.tgbotapi.utils.internal.ClassCastsIncluded
 import kotlinx.serialization.Serializable
 
 @Serializable
-@ClassCastsIncluded
 sealed interface GiveawayResults : WithPreviewChat, ChatEvent, PublicChatEvent {
     val unclaimedCount: Int
 }
