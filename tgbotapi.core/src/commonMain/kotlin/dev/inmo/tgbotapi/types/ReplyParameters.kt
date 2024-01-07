e28f3492efa054c89620518e70a8094b0a5f3453
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-01-07 21:54:56 +0600

    add ReplyParameters

---
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/ReplyParameters.kt
@@ -0,0 +1,121 @@
+package dev.inmo.tgbotapi.types
+
+import dev.inmo.tgbotapi.abstracts.TextedInput
+import dev.inmo.tgbotapi.abstracts.TextedWithTextSources
+import dev.inmo.tgbotapi.abstracts.WithMessageId
+import dev.inmo.tgbotapi.types.message.ParseMode
+import dev.inmo.tgbotapi.types.message.RawMessageEntity
+import dev.inmo.tgbotapi.types.message.abstracts.Message
+import dev.inmo.tgbotapi.types.message.asTextSources
+import dev.inmo.tgbotapi.types.message.textsources.TextSource
+import dev.inmo.tgbotapi.types.message.textsources.TextSourcesList
+import dev.inmo.tgbotapi.types.message.toRawMessageEntities
+import dev.inmo.tgbotapi.utils.extensions.makeSourceString
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ReplyParameters internal constructor(
+    @SerialName(chatIdField)
+    val chatIdentifier: ChatIdentifier,
+    @SerialName(messageIdField)
+    override val messageId: MessageId,
+    @SerialName(allowSendingWithoutReplyField)
+    val allowSendingWithoutReply: Boolean = false,
+    @SerialName(quoteField)
+    val quote: String? = null,
+    @SerialName(quoteParseModeField)
+    val quoteParseMode: ParseMode? = null,
+    @SerialName(quoteEntitiesField)
+    private val quoteEntities: List<RawMessageEntity>? = null,
+    @SerialName(quotePositionField)
+    val quotePosition: Int?
+) : WithMessageId, TextedInput {
+    override val text: String?
+        get() = quote
+    override val textSources: List<TextSource> by lazy {
+        quoteEntities ?.asTextSources(quote ?: return@lazy emptyList()) ?: emptyList()
+    }
+
+    constructor(
+        chatIdentifier: ChatIdentifier,
+        messageId: MessageId,
+        entities: TextSourcesList,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        chatIdentifier,
+        messageId,
+        allowSendingWithoutReply,
+        entities.makeSourceString(),
+        null,
+        entities.toRawMessageEntities(),
+        quotePosition
+    )
+    constructor(
+        metaInfo: Message.MetaInfo,
+        entities: TextSourcesList,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        metaInfo.chatId,
+        metaInfo.messageId,
+        entities,
+        allowSendingWithoutReply,
+        quotePosition
+    )
+    constructor(
+        chatIdentifier: ChatIdentifier,
+        messageId: MessageId,
+        quote: String,
+        quoteParseMode: ParseMode,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        chatIdentifier,
+        messageId,
+        allowSendingWithoutReply,
+        quote,
+        quoteParseMode,
+        null,
+        quotePosition
+    )
+    constructor(
+        metaInfo: Message.MetaInfo,
+        quote: String,
+        quoteParseMode: ParseMode,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        metaInfo.chatId,
+        metaInfo.messageId,
+        quote,
+        quoteParseMode,
+        allowSendingWithoutReply,
+        quotePosition
+    )
+    constructor(
+        chatIdentifier: ChatIdentifier,
+        messageId: MessageId,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        chatIdentifier,
+        messageId,
+        allowSendingWithoutReply,
+        null,
+        null,
+        null,
+        quotePosition
+    )
+    constructor(
+        metaInfo: Message.MetaInfo,
+        allowSendingWithoutReply: Boolean = false,
+        quotePosition: Int? = null
+    ) : this(
+        metaInfo.chatId,
+        metaInfo.messageId,
+        allowSendingWithoutReply,
+        quotePosition
+    )
+}
