66e31e5d0c8f6c01dbd67dac8f13b79b31d282b2
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-01-12 01:53:40 +0600

    fixes in AccessibleMessage -> Message in several old parts

--- tgbotapi.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/raw/Message.kt
+++ tgbotapi.utils/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/utils/extensions/raw/Message.kt
@@ -23,58 +23,58 @@ import dev.inmo.tgbotapi.types.venue.Venue
 import dev.inmo.tgbotapi.utils.RiskFeature
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.from: User?
+inline val Message.from: User?
     get() = asFromUser() ?.from
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.sender_chat: PublicChat?
+inline val Message.sender_chat: PublicChat?
     get() = asFromChannelGroupContentMessage() ?.senderChat
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_from: User?
+inline val Message.forward_from: User?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.asUserForwardInfo() ?.from
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_from_chat: Chat?
+inline val Message.forward_from_chat: Chat?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.asForwardFromPublicChatInfo() ?.chat
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_from_message_id: MessageId?
+inline val Message.forward_from_message_id: MessageId?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.fromChannelOrNull() ?.messageId
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_signature: ForwardSignature?
+inline val Message.forward_signature: ForwardSignature?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.fromChannelOrNull() ?.signature
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_sender_name: ForwardSenderName?
+inline val Message.forward_sender_name: ForwardSenderName?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.asAnonymousForwardInfo() ?.senderName
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.forward_date: TelegramDate?
+inline val Message.forward_date: TelegramDate?
     get() = asPossiblyForwardedMessage() ?.forwardInfo ?.dateOfOriginal
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.is_automatic_forward: Boolean?
+inline val Message.is_automatic_forward: Boolean?
     get() = this is ConnectedFromChannelGroupContentMessage<*>
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.reply_to_message: Message?
+inline val Message.reply_to_message: Message?
     get() = asPossiblyReplyMessage() ?.replyTo
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.via_bot: CommonBot?
+inline val Message.via_bot: CommonBot?
     get() = asPossiblySentViaBotCommonMessage() ?.senderBot
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.edit_date: TelegramDate?
+inline val Message.edit_date: TelegramDate?
     get() = asPossiblyEditedMessage() ?.editDate ?.toTelegramDate()
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.has_protected_content: Boolean?
+inline val Message.has_protected_content: Boolean?
     get() = asContentMessage() ?.hasProtectedContent
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.media_group_id: MediaGroupIdentifier?
+inline val Message.media_group_id: MediaGroupIdentifier?
     get() = asMediaGroupMessage() ?.mediaGroupId
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.author_signature: AuthorSignature?
+inline val Message.author_signature: AuthorSignature?
     get() = asSignedMessage() ?.authorSignature
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.text: String?
+inline val Message.text: String?
     get() = asContentMessage() ?.content ?.asTextContent() ?.text
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.entities: TextSourcesList?
+inline val Message.entities: TextSourcesList?
     get() = asContentMessage() ?.content ?.asTextContent() ?.textSources
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.caption: String?
+inline val Message.caption: String?
     get() = whenContentMessage {
         if (it.content !is TextContent) {
             it.content.asTextedInput() ?.text
@@ -83,7 +83,7 @@ inline val AccessibleMessage.caption: String?
         }
     }
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.caption_entities: TextSourcesList?
+inline val Message.caption_entities: TextSourcesList?
     get() = whenContentMessage {
         if (it.content !is TextContent) {
             it.content.asTextedInput() ?.textSources
@@ -92,117 +92,117 @@ inline val AccessibleMessage.caption_entities: TextSourcesList?
         }
     }
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.audio: AudioFile?
+inline val Message.audio: AudioFile?
     get() = asContentMessage() ?.content ?.asAudioContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.document: DocumentFile?
+inline val Message.document: DocumentFile?
     get() = asContentMessage() ?.content ?.asDocumentContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.animation: AnimationFile?
+inline val Message.animation: AnimationFile?
     get() = asContentMessage() ?.content ?.asAnimationContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.game: Game?
+inline val Message.game: Game?
     get() = asContentMessage() ?.content ?.asGameContent() ?.game
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.photo: Photo?
+inline val Message.photo: Photo?
     get() = asContentMessage() ?.content ?.asPhotoContent() ?.mediaCollection
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.sticker: Sticker?
+inline val Message.sticker: Sticker?
     get() = asContentMessage() ?.content ?.asStickerContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video: VideoFile?
+inline val Message.video: VideoFile?
     get() = asContentMessage() ?.content ?.asVideoContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.voice: VoiceFile?
+inline val Message.voice: VoiceFile?
     get() = asContentMessage() ?.content ?.asVoiceContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video_note: VideoNoteFile?
+inline val Message.video_note: VideoNoteFile?
     get() = asContentMessage() ?.content ?.asVideoNoteContent() ?.media
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.contact: Contact?
+inline val Message.contact: Contact?
     get() = asContentMessage() ?.content ?.asContactContent() ?.contact
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.location: Location?
+inline val Message.location: Location?
     get() = asContentMessage() ?.content ?.asLocationContent() ?.location
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.venue: Venue?
+inline val Message.venue: Venue?
     get() = asContentMessage() ?.content ?.asVenueContent() ?.venue
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.poll: Poll?
+inline val Message.poll: Poll?
     get() = asContentMessage() ?.content ?.asPollContent() ?.poll
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.invoice: Invoice?
+inline val Message.invoice: Invoice?
     get() = asContentMessage() ?.content ?.asInvoiceContent() ?.invoice
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.dice: Dice?
+inline val Message.dice: Dice?
     get() = asContentMessage() ?.content ?.asDiceContent() ?.dice
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.new_chat_members: List<User>?
+inline val Message.new_chat_members: List<User>?
     get() = asChatEventMessage() ?.chatEvent ?.asNewChatMembers() ?.members
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.left_chat_member: User?
+inline val Message.left_chat_member: User?
     get() = asChatEventMessage() ?.chatEvent ?.asLeftChatMember() ?.user
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.new_chat_title: String?
+inline val Message.new_chat_title: String?
     get() = asChatEventMessage() ?.chatEvent ?.asNewChatTitle() ?.title
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.new_chat_photo: Photo?
+inline val Message.new_chat_photo: Photo?
     get() = asChatEventMessage() ?.chatEvent ?.asNewChatPhoto() ?.photo
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.delete_chat_photo: Boolean
+inline val Message.delete_chat_photo: Boolean
     get() = asChatEventMessage() ?.chatEvent is DeleteChatPhoto
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.group_chat_created: Boolean
+inline val Message.group_chat_created: Boolean
     get() = asChatEventMessage() ?.chatEvent is GroupChatCreated
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.supergroup_chat_created: Boolean
+inline val Message.supergroup_chat_created: Boolean
     get() = asChatEventMessage() ?.chatEvent is SupergroupChatCreated
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.channel_chat_created: Boolean
+inline val Message.channel_chat_created: Boolean
     get() = asChatEventMessage() ?.chatEvent is ChannelChatCreated
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.migrate_to_chat_id: IdChatIdentifier?
+inline val Message.migrate_to_chat_id: IdChatIdentifier?
     get() = asChatEventMessage() ?.chatEvent ?.asGroupChatCreated() ?.migratedTo
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.migrate_from_chat_id: IdChatIdentifier?
+inline val Message.migrate_from_chat_id: IdChatIdentifier?
     get() = asChatEventMessage() ?.chatEvent ?.let {
          it ?.asSupergroupChatCreated() ?.migratedFrom ?: it ?.asMigratedToSupergroup() ?.migratedFrom
     }
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.pinned_message: Message?
+inline val Message.pinned_message: Message?
     get() = asChatEventMessage() ?.chatEvent ?.asPinnedMessage() ?.message
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.successful_payment: SuccessfulPayment?
+inline val Message.successful_payment: SuccessfulPayment?
     get() = asChatEventMessage() ?.chatEvent ?.asSuccessfulPaymentEvent() ?.payment
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video_chat_scheduled: VideoChatScheduled?
+inline val Message.video_chat_scheduled: VideoChatScheduled?
     get() = asChatEventMessage() ?.chatEvent ?.asVideoChatScheduled()
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video_chat_started: VideoChatStarted?
+inline val Message.video_chat_started: VideoChatStarted?
     get() = asChatEventMessage() ?.chatEvent ?.asVideoChatStarted()
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video_chat_ended: VideoChatEnded?
+inline val Message.video_chat_ended: VideoChatEnded?
     get() = asChatEventMessage() ?.chatEvent ?.asVideoChatEnded()
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.video_chat_participants_invited: VideoChatParticipantsInvited?
+inline val Message.video_chat_participants_invited: VideoChatParticipantsInvited?
     get() = asChatEventMessage() ?.chatEvent ?.asVideoChatParticipantsInvited()
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.message_auto_delete_timer_changed: MessageAutoDeleteTimerChanged?
+inline val Message.message_auto_delete_timer_changed: MessageAutoDeleteTimerChanged?
     get() = asChatEventMessage() ?.chatEvent ?.asMessageAutoDeleteTimerChanged()
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.connected_website: String?
+inline val Message.connected_website: String?
     get() = asChatEventMessage() ?.chatEvent ?.asUserLoggedIn() ?.domain
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.proximity_alert_triggered: ProximityAlertTriggered?
+inline val Message.proximity_alert_triggered: ProximityAlertTriggered?
     get() = asChatEventMessage() ?.chatEvent ?.asProximityAlertTriggered()
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.passport_data: PassportData?
+inline val Message.passport_data: PassportData?
     get() = asPassportMessage() ?.passportData
 
 @RiskFeature(RawFieldsUsageWarning)
-inline val AccessibleMessage.reply_markup: InlineKeyboardMarkup?
+inline val Message.reply_markup: InlineKeyboardMarkup?
     get() = asCommonMessage() ?.replyMarkup
