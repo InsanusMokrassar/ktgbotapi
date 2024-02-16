3da4ee48087caefa97a285fd76cf2792b4ccbe28
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-02-17 01:46:26 +0600

    make Story to be data class

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/stories/Story.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/stories/Story.kt
@@ -9,7 +9,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-class Story(
+data class Story(
     @SerialName(idField)
     val id: StoryId,
     @SerialName(chatField)
