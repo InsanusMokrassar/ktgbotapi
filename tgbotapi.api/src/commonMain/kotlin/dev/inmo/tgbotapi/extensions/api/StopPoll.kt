af79e268557c199d2e3a7a0da7e417a042b7e7db
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-06-23 15:23:58 +0600

    add support for businessConnectionId in stopPoll

--- tgbotapi.api/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/api/StopPoll.kt
+++ tgbotapi.api/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/api/StopPoll.kt
@@ -3,6 +3,7 @@ package dev.inmo.tgbotapi.extensions.api
 import dev.inmo.tgbotapi.bot.TelegramBot
 import dev.inmo.tgbotapi.requests.StopPoll
 import dev.inmo.tgbotapi.types.*
+import dev.inmo.tgbotapi.types.business_connection.BusinessConnectionId
 import dev.inmo.tgbotapi.types.buttons.InlineKeyboardMarkup
 import dev.inmo.tgbotapi.types.chat.Chat
 import dev.inmo.tgbotapi.types.message.abstracts.AccessibleMessage
@@ -14,9 +15,10 @@ import dev.inmo.tgbotapi.types.message.abstracts.AccessibleMessage
 suspend fun TelegramBot.stopPoll(
     chatId: ChatIdentifier,
     messageId: MessageId,
+    businessConnectionId: BusinessConnectionId? = chatId.businessConnectionId,
     replyMarkup: InlineKeyboardMarkup? = null
 ) = execute(
-    StopPoll(chatId, messageId, replyMarkup)
+    StopPoll(chatId, messageId, businessConnectionId, replyMarkup)
 )
 
 /**
@@ -26,8 +28,9 @@ suspend fun TelegramBot.stopPoll(
 suspend fun TelegramBot.stopPoll(
     chat: Chat,
     messageId: MessageId,
+    businessConnectionId: BusinessConnectionId? = chat.id.businessConnectionId,
     replyMarkup: InlineKeyboardMarkup? = null
-) = stopPoll(chat.id, messageId, replyMarkup)
+) = stopPoll(chat.id, messageId, businessConnectionId, replyMarkup)
 
 /**
  * @param replyMarkup Some [InlineKeyboardMarkup]. See [dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard]
@@ -36,8 +39,9 @@ suspend fun TelegramBot.stopPoll(
 suspend fun TelegramBot.stopPoll(
     chatId: IdChatIdentifier,
     message: AccessibleMessage,
+    businessConnectionId: BusinessConnectionId? = chatId.businessConnectionId,
     replyMarkup: InlineKeyboardMarkup? = null
-) = stopPoll(chatId, message.messageId, replyMarkup)
+) = stopPoll(chatId, message.messageId, businessConnectionId, replyMarkup)
 
 /**
  * @param replyMarkup Some [InlineKeyboardMarkup]. See [dev.inmo.tgbotapi.extensions.utils.types.buttons.inlineKeyboard]
@@ -46,5 +50,6 @@ suspend fun TelegramBot.stopPoll(
 suspend fun TelegramBot.stopPoll(
     chat: Chat,
     message: AccessibleMessage,
+    businessConnectionId: BusinessConnectionId? = chat.id.businessConnectionId,
     replyMarkup: InlineKeyboardMarkup? = null
-) = stopPoll(chat.id, message.messageId, replyMarkup)
+) = stopPoll(chat.id, message.messageId, businessConnectionId, replyMarkup)
