b7d3d849eb4b7fd81d3cd2487f98ad79db20e742
bpavuk <boopentiy@gmail.com>
2024-07-07 19:50:28 +0300

    optimization and conventions converted some objects with purpose to hold the data to data objects

--- tgbotapi.webapps/src/jsMain/kotlin/dev/inmo/tgbotapi/webapps/EventType.kt
+++ tgbotapi.webapps/src/jsMain/kotlin/dev/inmo/tgbotapi/webapps/EventType.kt
@@ -1,16 +1,16 @@
 package dev.inmo.tgbotapi.webapps
 
 sealed class EventType(val typeName: String) {
-    object ThemeChanged : EventType("themeChanged")
-    object ViewportChanged : EventType("viewportChanged")
-    object MainButtonClicked : EventType("mainButtonClicked")
-    object BackButtonClicked : EventType("backButtonClicked")
-    object SettingsButtonClicked : EventType("settingsButtonClicked")
-    object InvoiceClosed : EventType("invoiceClosed")
-    object PopupClosed : EventType("popupClosed")
-    object QRTextReceived : EventType("qrTextReceived")
-    object ClipboardTextReceived : EventType("clipboardTextReceived")
-    object WriteAccessRequested : EventType("writeAccessRequested")
-    object ContactRequested : EventType("contactRequested")
-    object ScanQRPopupClosed : EventType("scanQrPopupClosed")
+    data object ThemeChanged : EventType("themeChanged")
+    data object ViewportChanged : EventType("viewportChanged")
+    data object MainButtonClicked : EventType("mainButtonClicked")
+    data object BackButtonClicked : EventType("backButtonClicked")
+    data object SettingsButtonClicked : EventType("settingsButtonClicked")
+    data object InvoiceClosed : EventType("invoiceClosed")
+    data object PopupClosed : EventType("popupClosed")
+    data object QRTextReceived : EventType("qrTextReceived")
+    data object ClipboardTextReceived : EventType("clipboardTextReceived")
+    data object WriteAccessRequested : EventType("writeAccessRequested")
+    data object ContactRequested : EventType("contactRequested")
+    data object ScanQRPopupClosed : EventType("scanQrPopupClosed")
 }
