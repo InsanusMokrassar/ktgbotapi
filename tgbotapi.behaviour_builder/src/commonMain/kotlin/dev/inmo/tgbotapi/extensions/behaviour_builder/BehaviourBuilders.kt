610ada505f16635e7285128ae123ca67c702dc56
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-07-09 17:35:10 +0600

    add notice about privacy handling

--- tgbotapi.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/BehaviourBuilders.kt
+++ tgbotapi.behaviour_builder/src/commonMain/kotlin/dev/inmo/tgbotapi/extensions/behaviour_builder/BehaviourBuilders.kt
@@ -1,5 +1,6 @@
 package dev.inmo.tgbotapi.extensions.behaviour_builder
 
+import dev.inmo.kslog.common.e
 import dev.inmo.micro_utils.coroutines.ContextSafelyExceptionHandler
 import dev.inmo.micro_utils.coroutines.ExceptionHandler
 import dev.inmo.tgbotapi.bot.TelegramBot
@@ -8,6 +9,7 @@ import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.startGettingOfUpdat
 import dev.inmo.tgbotapi.extensions.utils.updates.retrieving.updateHandlerWithMediaGroupsAdaptation
 import dev.inmo.tgbotapi.types.Seconds
 import dev.inmo.tgbotapi.updateshandlers.FlowsUpdatesFilter
+import dev.inmo.tgbotapi.utils.DefaultKTgBotAPIKSLog
 import kotlinx.coroutines.*
 
 /**
@@ -70,6 +72,9 @@ suspend fun TelegramBot.buildBehaviourWithLongPolling(
         defaultExceptionsHandler = defaultExceptionsHandler,
         block = block
     )
+    if (!behaviourContext.triggersHolder.handleableCommandsHolder.isHandled("privacy")) {
+        DefaultKTgBotAPIKSLog.e { "For some or bots there is no handling \"privacy\" command. According to https://telegram.org/tos/bot-developers#4-privacy it may lead to bot deactivation or removing" }
+    }
     return longPolling(
         behaviourContext,
         scope = behaviourContext,
