5d8deb4b9b8eab8c254b991a197b8606d6857c30
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-09-07 00:07:59 +0600

    add is_star_giveaway into GiveawayPrivateResults

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayPrivateResults.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayPrivateResults.kt
@@ -1,16 +1,24 @@
 package dev.inmo.tgbotapi.types.giveaway
 
+import dev.inmo.tgbotapi.types.*
 import dev.inmo.tgbotapi.types.chat.PreviewChat
 import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.ChatEvent
 import dev.inmo.tgbotapi.types.message.ChatEvents.abstracts.PublicChatEvent
 import dev.inmo.tgbotapi.types.message.abstracts.AccessibleMessage
+import dev.inmo.tgbotapi.types.message.abstracts.TelegramBotAPIMessageDeserializeOnlySerializer
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
 
 @Serializable
 data class GiveawayPrivateResults(
+    @SerialName(chatField)
     override val chat: PreviewChat,
+    @SerialName(unclaimedPrizeCountField)
     override val unclaimedCount: Int,
-    @Transient // TODO::Add message serializer
-    val message: AccessibleMessage? = null
+    @SerialName(giveawayMessageField)
+    @Serializable(TelegramBotAPIMessageDeserializeOnlySerializer::class)
+    val message: AccessibleMessage? = null,
+    @SerialName(isStarGiveawayField)
+    val isStarGiveaway: Boolean = false,
 ) : GiveawayResults, ChatEvent, PublicChatEvent
