d347c94c8bd24a6d5f733779193907aa44bd1c64
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-09-07 00:02:09 +0600

    revoke rework of GiveawayPublicResults

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayPublicResults.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/giveaway/GiveawayPublicResults.kt
@@ -10,7 +10,7 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 @Serializable(GiveawayPublicResults.Companion::class)
-sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPreviewChatAndMessageId,
+sealed interface GiveawayPublicResults : GiveawayInfo, GiveawayResults, WithPreviewChatAndMessageId,
     ReplyInfo.External.ContentVariant, GiveawayInfo.OptionallyStars, GiveawayInfo.OptionallyPremium {
     val count: Int
     val winners: List<PreviewUser>
@@ -53,74 +53,8 @@ sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPrevi
             get() = null
     }
 
-    sealed interface Winners : GiveawayPublicResults {
-        override val prizeStarCount: Int?
-            get() = null
-        override val premiumMonths: Int?
-            get() = null
-        @Serializable
-        data class Premium (
-            @SerialName(chatsField)
-            override val chat: PreviewChat,
-            @SerialName(giveawayMessageIdField)
-            override val messageId: MessageId,
-            @SerialName(winnersSelectionDateField)
-            override val selectionDate: TelegramDate,
-            @SerialName(winnersCountField)
-            override val count: Int,
-            @SerialName(winnersField)
-            override val winners: List<PreviewUser>,
-            @SerialName(premiumSubscriptionMonthCountField)
-            override val premiumMonths: Int,
-            @SerialName(additionalChatCountField)
-            override val additionalChats: Int = 0,
-            @SerialName(unclaimedPrizeCountField)
-            override val unclaimedCount: Int = 0,
-            @SerialName(onlyNewMembersField)
-            override val onlyNewMembers: Boolean = false,
-            @SerialName(hasPublicWinnersField)
-            override val publicWinners: Boolean = false,
-            @SerialName(prizeDescriptionField)
-            override val additionalPrizeDescription: String? = null,
-        ) : Winners, GiveawayInfo.Premium {
-            @SerialName(wasRefundedField)
-            @Required
-            @EncodeDefault
-            override val refunded: Boolean = false
-        }
-        @Serializable
-        data class Stars (
-            @SerialName(chatsField)
-            override val chat: PreviewChat,
-            @SerialName(giveawayMessageIdField)
-            override val messageId: MessageId,
-            @SerialName(winnersSelectionDateField)
-            override val selectionDate: TelegramDate,
-            @SerialName(winnersCountField)
-            override val count: Int,
-            @SerialName(winnersField)
-            override val winners: List<PreviewUser>,
-            @SerialName(prizeStarCountField)
-            override val prizeStarCount: Int,
-            @SerialName(additionalChatCountField)
-            override val additionalChats: Int = 0,
-            @SerialName(unclaimedPrizeCountField)
-            override val unclaimedCount: Int = 0,
-            @SerialName(onlyNewMembersField)
-            override val onlyNewMembers: Boolean = false,
-            @SerialName(hasPublicWinnersField)
-            override val publicWinners: Boolean = false,
-            @SerialName(prizeDescriptionField)
-            override val additionalPrizeDescription: String? = null,
-        ) : Winners, GiveawayInfo.Stars {
-            @SerialName(wasRefundedField)
-            @Required
-            @EncodeDefault
-            override val refunded: Boolean = false
-        }
-    }
     @Serializable
-    data class Unknown (
+    data class Winners(
         @SerialName(chatsField)
         override val chat: PreviewChat,
         @SerialName(giveawayMessageIdField)
@@ -131,10 +65,6 @@ sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPrevi
         override val count: Int,
         @SerialName(winnersField)
         override val winners: List<PreviewUser>,
-        @SerialName(prizeStarCountField)
-        override val prizeStarCount: Int?,
-        @SerialName(premiumSubscriptionMonthCountField)
-        override val premiumMonths: Int?,
         @SerialName(additionalChatCountField)
         override val additionalChats: Int = 0,
         @SerialName(unclaimedPrizeCountField)
@@ -145,6 +75,10 @@ sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPrevi
         override val publicWinners: Boolean = false,
         @SerialName(prizeDescriptionField)
         override val additionalPrizeDescription: String? = null,
+        @SerialName(premiumSubscriptionMonthCountField)
+        override val premiumMonths: Int? = null,
+        @SerialName(prizeStarCountField)
+        override val prizeStarCount: Int? = null,
     ) : GiveawayPublicResults {
         @SerialName(wasRefundedField)
         @Required
@@ -195,33 +129,7 @@ sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPrevi
                     messageId = surrogate.messageId,
                     selectionDate = surrogate.selectionDate
                 )
-                surrogate.premiumMonths != null -> Winners.Premium(
-                    chat = surrogate.chat,
-                    messageId = surrogate.messageId,
-                    selectionDate = surrogate.selectionDate,
-                    count = surrogate.count,
-                    winners = surrogate.winners,
-                    additionalChats = surrogate.additionalChats,
-                    unclaimedCount = surrogate.unclaimedCount,
-                    onlyNewMembers = surrogate.onlyNewMembers,
-                    publicWinners = surrogate.publicWinners,
-                    additionalPrizeDescription = surrogate.additionalPrizeDescription,
-                    premiumMonths = surrogate.premiumMonths,
-                )
-                surrogate.starsCount != null -> Winners.Stars(
-                    chat = surrogate.chat,
-                    messageId = surrogate.messageId,
-                    selectionDate = surrogate.selectionDate,
-                    count = surrogate.count,
-                    winners = surrogate.winners,
-                    additionalChats = surrogate.additionalChats,
-                    unclaimedCount = surrogate.unclaimedCount,
-                    onlyNewMembers = surrogate.onlyNewMembers,
-                    publicWinners = surrogate.publicWinners,
-                    additionalPrizeDescription = surrogate.additionalPrizeDescription,
-                    prizeStarCount = surrogate.starsCount,
-                )
-                else -> Unknown(
+                else -> Winners(
                     chat = surrogate.chat,
                     messageId = surrogate.messageId,
                     selectionDate = surrogate.selectionDate,
@@ -257,46 +165,5 @@ sealed interface GiveawayPublicResults: GiveawayInfo, GiveawayResults, WithPrevi
 
             Surrogate.serializer().serialize(encoder, surrogate)
         }
-
-        fun Winners(
-            chat: PreviewChat,
-            messageId: MessageId,
-            selectionDate: TelegramDate,
-            count: Int,
-            winners: List<PreviewUser>,
-            additionalChats: Int = 0,
-            unclaimedCount: Int = 0,
-            onlyNewMembers: Boolean = false,
-            publicWinners: Boolean = false,
-            additionalPrizeDescription: String? = null,
-            premiumMonths: Int? = null
-        ) = premiumMonths ?.let {
-            Winners.Premium(
-                chat = chat,
-                messageId = messageId,
-                selectionDate = selectionDate,
-                count = count,
-                winners = winners,
-                premiumMonths = premiumMonths,
-                additionalChats = additionalChats,
-                unclaimedCount = unclaimedCount,
-                onlyNewMembers = onlyNewMembers,
-                publicWinners = publicWinners,
-                additionalPrizeDescription = additionalPrizeDescription
-            )
-        } ?: Unknown(
-            chat = chat,
-            messageId = messageId,
-            selectionDate = selectionDate,
-            count = count,
-            winners = winners,
-            prizeStarCount = null,
-            premiumMonths = premiumMonths,
-            additionalChats = additionalChats,
-            unclaimedCount = unclaimedCount,
-            onlyNewMembers = onlyNewMembers,
-            publicWinners = publicWinners,
-            additionalPrizeDescription = additionalPrizeDescription
-        )
     }
 }
