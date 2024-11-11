fb3b8a741592c329586b4d06a0b85a301c9e1796
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-11-11 13:03:46 +0600

    fix of #917

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/payments/OrderInfo.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/payments/OrderInfo.kt
@@ -4,14 +4,21 @@ import dev.inmo.tgbotapi.types.*
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * All the field of this class are nullable due to specific of [OrderInfo](https://core.telegram.org/bots/api#orderinfo)
+ * from official bots api
+ *
+ * @see dev.inmo.tgbotapi.abstracts.CommonSendInvoiceData
+ * @see dev.inmo.tgbotapi.requests.send.payments.SendInvoice
+ */
 @Serializable
 data class OrderInfo(
     @SerialName(nameField)
-    val name: String,
+    val name: String?,
     @SerialName(phoneNumberField)
-    val phoneNumber: String,
+    val phoneNumber: String?,
     @SerialName(emailField)
-    val email: String,
+    val email: String?,
     @SerialName(shippingAddressField)
-    val shippingAddress: ShippingAddress
+    val shippingAddress: ShippingAddress?
 )
