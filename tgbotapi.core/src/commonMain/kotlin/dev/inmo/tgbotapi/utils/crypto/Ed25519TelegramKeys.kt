4cdae11adc480c178bf52e584a9cd9bba9893f87
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2024-12-06 12:26:11 +0600

    support of Ed25519TelegramKeys

---
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/utils/crypto/Ed25519TelegramKeys.kt
@@ -0,0 +1,6 @@
+package dev.inmo.tgbotapi.utils.crypto
+
+object Ed25519TelegramKeys {
+    const val TestEnvironment = "40055058a4ee38156a06562e52eece92a771bcd8346a8c4615cb7376eddf72ec" // hex
+    const val Production = "e7bf03a2fa4602af4580703d88dda5bb59f32ed8b02a56c187fe7d34caed242d" // hex
+}
