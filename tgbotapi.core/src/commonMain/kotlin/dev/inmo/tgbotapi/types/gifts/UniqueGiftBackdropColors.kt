314c8d9ad1415e9aecfca708eb6166ea9e895a8a
bpavuk <75901693+bpavuk@users.noreply.github.com>
2025-04-13 16:34:41 +0300

    fix: using RGBColor abstractions instead of raw Int values now.

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/gifts/UniqueGiftBackdropColors.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/gifts/UniqueGiftBackdropColors.kt
@@ -4,6 +4,7 @@ import dev.inmo.tgbotapi.types.centerColorField
 import dev.inmo.tgbotapi.types.edgeColorField
 import dev.inmo.tgbotapi.types.symbolColorField
 import dev.inmo.tgbotapi.types.textColorField
+import dev.inmo.tgbotapi.utils.RGBColor
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
@@ -18,11 +19,11 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class UniqueGiftBackdropColors(
     @SerialName(centerColorField)
-    val centerColor: Int,
+    val centerColor: RGBColor,
     @SerialName(edgeColorField)
-    val edgeColor: Int,
+    val edgeColor: RGBColor,
     @SerialName(symbolColorField)
-    val symbolColor: Int,
+    val symbolColor: RGBColor,
     @SerialName(textColorField)
-    val textColor: Int
+    val textColor: RGBColor
 )
