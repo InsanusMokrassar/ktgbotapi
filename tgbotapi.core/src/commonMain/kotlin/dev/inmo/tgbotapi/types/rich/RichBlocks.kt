b054f3d1b5a46a81419b29d1673efbcafee49b45
InsanusMokrassar <ovsyannikov.alexey95@gmail.com>
2026-07-01 11:25:43 +0600

    Revert "Modify markdown function to add newlines"
    This reverts commit 3a05a88d4fb7333ed89dfaf63c3265fdd13a1171.

--- tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/rich/RichBlocks.kt
+++ tgbotapi.core/src/commonMain/kotlin/dev/inmo/tgbotapi/types/rich/RichBlocks.kt
@@ -54,7 +54,7 @@ data class RichBlockParagraph(
 
     companion object {
         const val TYPE = "paragraph"
-        fun markdown(text: RichText): String = "\n${text.markdown}\n"
+        fun markdown(text: RichText): String = text.markdown
         fun html(text: RichText): String = "<p>${text.html}</p>"
     }
 }
